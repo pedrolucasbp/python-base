db07812ce1017cf8f27b9493b70090f5e8722e94
Pedro Lucas <pedrolucasbp@gmail.com>
2023-10-25 14:07:02 +0200

    Adds multilingual messages

--- hello.py
+++ hello.py
@@ -1,2 +1,39 @@
 #!/usr/bin/env python3
-print ("Hello, world!")
+""" This script is a hello world multi lingual.
+
+  Depends on the env LANG variabel this script show a "Hello, world" message
+  in this language
+
+ Usage:
+    set LANG variabel
+ Example:
+    python3 hello.py
+    or
+    ./hello.py
+ 
+"""
+__version__ = "0.0.1"
+__author__ = "Pedro Lucas"
+__license__ = "GPLv2"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg="Hello, world!"
+
+if current_language == "pt_BR":
+   msg = "Olá, Mundo!"
+
+elif current_language == "it_IT":
+   msg = "Ciao, Mondo!"
+
+elif current_language == "es_SP":
+   msg = "Hola, Mundo!"
+
+elif current_language == "de_DE":
+   msg = "Hallo Welt!"
+
+elif current_language == "fr_FR":
+   msg = "Bonjour Monde!"
+
+print(msg)
